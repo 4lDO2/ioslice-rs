f21727ba0cc0dc67f451883feb25531acfe9ad23
4lDO2 <4lDO2@protonmail.com>
2020-10-11 19:10:57 +0200

    make progress

--- src/buffer.rs
+++ src/buffer.rs
@@ -166,7 +166,12 @@ pub struct Buffer<T> {
     // TODO: Use a trait to omit this field when the type already is initialized.
     // NOTE: We use this counter __only internally__. Namely, in order to avoid unnecessary bounds
     // checking when indexing buffers, this must not be changed to any arbitrary value.
-    bytes_filled: usize,
+    // TODO: Use some integer generic type (or perhaps simply a platform-specific type alias), to
+    // reduce the memory footprint when IOV_MAX happens to be less than 65536. Or at least just
+    // 32-bit.
+    bytes_filled_for_vector: usize,
+    // TODO: Use specialization to omit this field when there is only one buffer..
+    vectors_filled: usize,
 }
 pub struct BufferRef<'buffer, T> {
     buffer: &'buffer mut Buffer<T>,
@@ -177,7 +182,8 @@ impl<T> Buffer<T> {
     pub const fn new(inner: T) -> Self {
         Self {
             inner,
-            bytes_filled: 0,
+            bytes_filled_for_vector: 0,
+            vectors_filled: 0,
         }
     }
     #[inline]
@@ -187,7 +193,11 @@ impl<T> Buffer<T> {
 
     #[inline]
     pub fn bytes_filled(&self) -> usize {
-        self.bytes_filled
+        self.bytes_filled_for_vector
+    }
+    #[inline]
+    pub fn vectors_filled(&self) -> usize {
+        self.vectors_filled
     }
 
     #[inline]
@@ -202,46 +212,56 @@ impl<T> Buffer<T>
 where
     T: InitializeIndirect,
 {
+    #[inline]
+    fn all_uninit(&self) -> &[T::UninitItem] {
+        self.inner.as_maybe_uninit_slices()
+    }
+
+    #[inline]
+    pub fn current_buffer(&self) -> Option<&[MaybeUninit<u8>]> {
+        Some(self.inner.as_maybe_uninit_slices().get(self.vectors_filled)?.as_maybe_uninit_slice())
+    }
     fn debug_assert_valid_len(&self) {
-        debug_assert!(self.all_uninit().len() >= self.bytes_filled);
-        debug_assert!(self.bytes_filled <= isize::MAX as usize);
+        debug_assert!(self.current_buffer().map_or(true, |current_buffer| current_buffer.len() >= self.bytes_filled_for_vector));
+        debug_assert!(self.bytes_filled_for_vector <= isize::MAX as usize);
+        debug_assert!(self.inner.as_maybe_uninit_slices().len() >= self.vectors_filled);
     }
 
     #[inline]
-    pub fn all_uninit(&self) -> &[MaybeUninit<u8>] {
-        self.inner.as_maybe_uninit_slice()
+    pub fn total_vector_count(&self) -> usize {
+        self.inner.as_maybe_uninit_slices().len()
     }
-    #[inline]
-    pub fn bytes_to_fill(&self) -> usize {
-        self.debug_assert_valid_len();
 
-        self.all_uninit().len().wrapping_sub(self.bytes_filled)
+    #[inline]
+    pub fn vectors_to_fill(&self) -> usize {
+        self.total_vector_count().wrapping_sub(self.vectors_filled())
+    }
+
+    /// Counts the bytes that must be filled before the whole buffer is initialized.
+    #[inline]
+    pub fn count_bytes_to_fill(&self) -> usize {
+        self.all_uninit_slices().iter().map(|buffer| buffer.as_maybe_uninit_slice().len()).sum()
+    }
+
+    #[inline]
+    pub fn all_uninit_slices(&self) -> &[T::UninitItem] {
+        self.inner.as_maybe_uninit_slices()
     }
     #[inline]
     pub fn is_completely_filled(&self) -> bool {
-        self.bytes_to_fill() == 0
+        self.vectors_filled() == self.vectors_to_fill()
     }
     #[inline]
     pub fn is_partially_filled(&self) -> bool {
-        self.bytes_filled() > 0
+        self.vectors_filled() > 0 || self.bytes_filled_for_vector > 0
     }
     #[inline]
-    pub fn try_full_initialized(&self) -> Option<&[u8]> {
-        if self.is_completely_filled() {
-            Some(self.init_part())
-        } else {
-            None
-        }
-    }
-    #[inline]
-    pub fn uninit_part(&self) -> &[MaybeUninit<u8>] {
-        let all = self.all_uninit();
-        dbg!(all.len());
-        dbg!(self.bytes_filled);
+    pub fn current_vector_uninit_part(&self) -> Option<&[MaybeUninit<u8>]> {
+        let all = self.current_buffer()?;
 
         // NOTE: We use unsafe to eliminate unnecessary bounds checking. This may be negligible for
         // many scenarios, but we want to keep this interface zero-cost.
-        unsafe {
+        Some(unsafe {
             // Validate that bytes_filled is valid, when _debug assertions_ are enabled.
             self.debug_assert_valid_len();
 
@@ -255,8 +275,8 @@ where
             // 3) the resulting pointer cannot overflow the pointer size. This is also impossible,
             //    since for `all` to be a valid slice, it must not wrap around in address space,
             //    between its start and end range.
-            let ptr = all.as_ptr().add(self.bytes_filled);
-            let len = all.len().wrapping_sub(self.bytes_filled);
+            let ptr = all.as_ptr().add(self.bytes_filled_for_vector);
+            let len = all.len().wrapping_sub(self.bytes_filled_for_vector);
 
             // SAFETY: This is safe, because:
             //
@@ -273,19 +293,19 @@ where
             // 4) the number of bytes filled is not larger than isize::MAX. This is checked every
             //    time bytes_filled changes.
             core::slice::from_raw_parts(ptr, len)
-        }
+        })
     }
     #[inline]
-    pub fn init_part(&self) -> &[u8] {
-        let all = self.all_uninit();
+    pub fn current_vector_init_part(&self) -> Option<&[u8]> {
+        let all = self.current_buffer()?;
 
         // NOTE: Use of unsafe is only to eliminate bounds checks and maintain zero-cost.
-        unsafe {
+        Some(unsafe {
             // Validate that bytes_filled is valid, when _debug assertions_ are enabled.
             self.debug_assert_valid_len();
 
             let ptr = all.as_ptr();
-            let len = self.bytes_filled;
+            let len = self.bytes_filled_for_vector;
 
             // SAFETY: This is safe, due to the same invariants as with `uninit_part`, except for
             // the initialization invariant. We uphold this, by guaranteeing that the entire slice
@@ -294,57 +314,62 @@ where
             // case, since we know that bytes_filled must be smaller than or equal to the size of
             // the slice.
             core::slice::from_raw_parts(ptr as *const u8, len)
-        }
+        })
     }
     #[inline]
-    fn all_uninit_mut(&mut self) -> &mut [MaybeUninit<u8>] {
-        self.inner.as_maybe_uninit_slice_mut()
+    fn all_uninit_mut(&mut self) -> &mut [T::UninitItem] {
+        self.inner.as_maybe_uninit_slices_mut()
     }
 
     #[inline]
-    pub fn uninit_part_mut(&mut self) -> &mut [MaybeUninit<u8>] {
+    pub fn current_vector_mut(&mut self) -> Option<&mut [MaybeUninit<u8>]> {
+        Some(self.inner.as_maybe_uninit_slices_mut().get(self.vectors_filled)?.as_maybe_uninit_slice_mut())
+    }
+
+    #[inline]
+    pub fn current_vector_uninit_part_mut(&mut self) -> Option<&mut [MaybeUninit<u8>]> {
         // NOTE: We extract this to avoid multiple mutable aliases.
         let (orig_ptr, orig_len) = {
-            let all = self.all_uninit_mut();
+            let all = self.current_vector_mut()?;
 
             (all.as_mut_ptr(), all.len())
         };
 
-        unsafe {
+        Some(unsafe {
             // Validate that bytes_filled is correct when debug assertions are enabled.
             self.debug_assert_valid_len();
 
             // SAFETY: This pointer arithmetic operation, is safe for the same reasons as with
             // `uninit_part`.
-            let ptr = orig_ptr.add(self.bytes_filled);
+            let ptr = orig_ptr.add(self.bytes_filled_for_vector);
 
-            let len = orig_len.wrapping_sub(self.bytes_filled);
+            let len = orig_len.wrapping_sub(self.bytes_filled_for_vector);
 
             // SAFETY: This is safe for the exact same reasons as with `uninit_part`, but that
             // there must not be any reference _at all_ to the inner slice. This is upheld by
             // knowing that we have already borrowed the owner of the slice mutably.
             core::slice::from_raw_parts_mut(ptr, len)
-        }
+        })
     }
 
     #[inline]
-    pub fn init_part_mut(&mut self) -> &mut [u8] {
+    pub fn current_vector_init_part_mut(&mut self) -> Option<&mut [u8]> {
         // NOTE: We extract pointer+len in a separate block, to avoid creating multiple mutable
         // references to the same data.
         let orig_ptr = {
-            let all = self.all_uninit_mut();
+            let all = self.current_vector_mut()?;
             all.as_mut_ptr()
         };
 
-        unsafe {
+        Some(unsafe {
             let ptr = orig_ptr;
-            let len = self.bytes_filled;
+            let len = self.bytes_filled_for_vector;
 
             // SAFETY: This is safe for the exact same reasons as with `init_part`, except that we
             // also ensure that there is no access whatsoever to the inner data, since we are
             // borrowing `self` mutably.
             core::slice::from_raw_parts_mut(ptr as *mut u8, len)
-        }
+        })
     }
     /// Advance the internal cursor, that divides the initialized and uninitialized parts.
     ///
@@ -362,15 +387,30 @@ where
     /// will panic if the count, when added with the current number of bytes filled, neither
     /// overflows `isize::MAX`, nor the total size of the slice.
     #[inline]
-    pub unsafe fn advance(&mut self, count: usize) {
-        debug_assert!(self.bytes_filled.checked_add(count).map_or(false, |sum| sum <= isize::MAX as usize));
+    pub unsafe fn advance(&mut self, mut count: usize) -> usize {
+        let mut bytes_advanced = 0;
 
-        if count > isize::MAX as usize {
-            core::hint::unreachable_unchecked();
+        loop {
+            let current_uninit_part_len = match self.current_vector_uninit_part() {
+                Some(slice) => slice.len(),
+                None => break,
+            };
+            if count >= current_uninit_part_len {
+                self.vectors_filled = self.vectors_filled
+                    .checked_add(1)
+                    .expect("reached usize::MAX when incrementing the buffer index");
+                self.bytes_filled_for_vector = 0;
+
+                count -= current_uninit_part_len;
+
+                bytes_advanced -= current_uninit_part_len;
+                continue;
+            } else {
+                self.bytes_filled_for_vector += current_uninit_part_len;
+            }
         }
-        self.bytes_filled = self.bytes_filled.wrapping_add(count);
 
-        self.debug_assert_valid_len();
+        bytes_advanced
     }
 
     /// Advance the internal cursor to the end, marking the entire buffer as initialized.
@@ -383,8 +423,7 @@ where
     /// This is unsafe for the sole reason that every byte must have been written to for this to be
     /// safe.
     #[inline]
-    pub unsafe fn advance_to_end(&mut self) {
-        self.bytes_filled = self.uninit_part().len();
+    pub unsafe fn advance_to_end_of_vector(&mut self) {
     }
     /// Assume that the entire buffer has been initialized, returning the inner initialized type.
     ///
@@ -443,7 +482,8 @@ impl<'a> Buffer<&'a mut [u8]> {
     #[inline]
     pub fn from_slice_mut(slice: &'a mut [u8]) -> Self {
         Self {
-            bytes_filled: slice.len(),
+            bytes_filled_for_vector: slice.len(),
+            vectors_filled: 0,
             inner: slice,
         }
     }
